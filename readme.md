1dfd553f02ee46a5ca9a135741204167408471a2
devjanaprime <dev@primeacademy.io>
2016-08-10 16:34:22 -0500

    first!

---
+++ readme.md
@@ -0,0 +1,4 @@
+JQuery AJAX JSON Example
+========================
+
+Something simple that uses JQuery and AJAX to get data from a JSON file (hosted elsewhere) for display on the DOM. More of an example than an intro, really, but I think the code has been commented enough to help hand hold someone who is new to AJAX and JSON, but familiar with Javascript and JQuery through the process.
---
+++ scripts/getJsonExample.js
@@ -0,0 +1,56 @@
+var currentIndex=0;
+var theData;
+
+$(document).ready( function(){
+  console.log( 'in start' );
+  $.ajax({
+  url: 'http://devjana.net/support/records.json',
+  dataType: 'json',
+  success: function( data ){
+    theData = data;
+    displayStudent();
+   }, // end success
+  statusCode: {
+    404: function(){
+       alert( 'error connecting to server' );
+    } // end 404
+   } // end statusCode
+  }); // end ajax  object
+
+  $('#prevButton' ).click( function(){
+    currentIndex--;
+    // check if index is less than 0, if so go to lat record
+    if( currentIndex < 0 ){
+      currentIndex = theData.records.length - 1;
+    }
+    displayStudent();
+  }); // end prevButton click
+
+  $('#nextButton' ).click( function(){
+    currentIndex++;
+    // check if index is greater than or equal to length of array, if so go to first record
+    if( currentIndex >= theData.records.length ){
+      currentIndex = 0;
+    }
+    displayStudent();
+  }); // end prevButton click
+
+  var displayStudent = function(){
+    // empty output before we append new info
+    $("#outputDiv").empty();
+    // format student info
+    var nameOut = theData.records[ currentIndex ].first_field + " " + theData.records[ currentIndex ].last_field;
+    // format record number
+    var adjustedIndex = currentIndex +1;
+    var counterOut = adjustedIndex + "/" + theData.records.length;
+    /// - create element append - ///
+    // format output
+    var newHeader = document.createElement('h2');
+    newHeader.textContent=nameOut;
+    var newParagraph = document.createElement('p');
+    newParagraph.textContent= counterOut;
+    // display output
+    $("#outputDiv").append( newHeader );
+    $("#outputDiv").append( newParagraph );
+  }; // end display student
+}); // end doc ready
